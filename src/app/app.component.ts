84d470bc057ae5bba6e69a435432ef7568f021c6
Ashraf Saker <engashrafsaker@gmail.com>
2024-06-23 17:32:41 +0300

    added form and ngmodels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from '../header/header.component'
 import { NotesListComponent } from '../notes-list/notes-list.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,NotesListComponent],
+  imports: [RouterOutlet,HeaderComponent,NotesListComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'notes';
+
 }
