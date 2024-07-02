2516d44a32ea428e84eef63469aa9f3bf2116535
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-02 16:21:25 +0300

    Routing Setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter,Routes } from '@angular/router';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
-bootstrapApplication(AppComponent, appConfig)
+const routes: Routes = [
+  { path: '', component:NotesListComponent  },
+  { path: 'New', component:AddNoteComponent  },
+
+]
+
+bootstrapApplication(AppComponent,{
+  providers: [provideRouter(routes)],
+})
   .catch((err) => console.error(err));
--- src/notes-list/notes-list.component.ts
+++ src/notes-list/notes-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NOTES } from '../notes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.css'
 })
