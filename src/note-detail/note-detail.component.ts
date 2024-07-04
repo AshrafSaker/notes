526876ac15f2a7038516e3adedfeeb7be6c6564d
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-04 16:23:55 +0300

    note-detail
    new component created to open a note in a page

---
+++ src/note-detail/note-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-detail.component.html',
+  styleUrl: './note-detail.component.css'
+})
+export class NoteDetailComponent {
+
+}
