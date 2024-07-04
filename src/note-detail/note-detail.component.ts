754a53d7102a76b13171b5e05a89839b80fb6e5d
Ashraf Saker <engashrafsaker@gmail.com>
2024-07-04 16:35:03 +0300

    edit note-detail

--- src/note-detail/note-detail.component.ts
+++ src/note-detail/note-detail.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { NOTES } from '../notes';
+import { Router,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-note-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './note-detail.component.html',
   styleUrl: './note-detail.component.css'
 })
